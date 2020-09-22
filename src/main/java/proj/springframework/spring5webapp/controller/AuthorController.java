615c71873884d129c2a9ab87ab68fa4149783f2a
Diana <diana.barnea@gmail.com>
2020-09-22 18:51:27 +0300

    thymeleaf example - authors

---
+++ src/main/java/proj/springframework/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package proj.springframework.spring5webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import proj.springframework.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+    AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
